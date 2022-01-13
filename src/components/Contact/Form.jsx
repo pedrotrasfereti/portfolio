6293e82ec32b865057a822c26e9d0189fbb43568
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-01-13 00:05:14 -0300

    emailjs

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -1,37 +1,82 @@
 /*-===================== React =====================-*/
-import React from 'react'
+import React, { useState } from 'react'
 
 /*-===================== React =====================-*/
 import { BiSend as Send } from 'react-icons/bi'
 
+/*-===================== Services =====================-*/
+import emailjs from 'emailjs-com';
+
 const Form = () => {
+  const defaultValue = {
+    name: '',
+    address: '',
+    message: '',
+  }
+
+  const [values, setValues] = useState(defaultValue)
+
+  const handleInput = ({ target }) => {
+    const { name, value } = target
+
+    setValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }))
+
+    console.log(values)
+  }
+
+  const sendEmail = (evt) => {
+    evt.preventDefault();
+
+    emailjs.send(
+      'service_wlf213b',
+      'template_s64fl59',
+      values,
+      'user_uep8KlB8YFkDiihWTPbZq'
+    )
+      .then((result) => {
+        console.log(result)
+      }, (err) => {
+        console.log(err)
+      })
+
+    setValues(defaultValue)
+  };
+
   return (
-    <form className="Form">
+    <form
+      onSubmit={(evt) => sendEmail(evt)}
+      className="Form"
+    >
       <input
-        id="name"
+        name="name"
         type="text"
         className="Input"
         placeholder="Nome"
+        value={values.name}
+        onChange={(evt) => handleInput(evt)}
       />
 
       <input
-        id="email"
+        name="address"
         type="text"
         className="Input"
         placeholder="E-Mail"
+        value={values.address}
+        onChange={(evt) => handleInput(evt)}
       />
 
       <textarea
-        id="message"
+        name="message"
         className="TextArea"
         placeholder="Mensagem"
+        value={values.message}
+        onChange={(evt) => handleInput(evt)}
       />
 
-      <button
-        type="submit"
-        className="Button"
-        onClick={() => handleClick()}
-      >
+      <button type="submit" className="Button">
         Enviar <Send className="Icon" />
       </button>
     </form>
