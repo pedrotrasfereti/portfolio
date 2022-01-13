af107d2c70d1dd63eb9dd6f7faa13a33f89f9e5d
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-01-13 18:48:56 -0300

    formulario completo 2

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
             <div className="Container">
               <h2 className="Name">Local</h2>
               <span className="Text">
-                R. Gago Coutinho, 10 - Campinas/SP
+                Campinas/SP
               </span>
             </div>
           </div>
